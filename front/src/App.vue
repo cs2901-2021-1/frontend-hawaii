a00c86b0d7d5f1fad3c9a5ae238be971ed3f683b
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-07-09 20:20:39 -0500

    Starting Vue Template

---
+++ front/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app>
+
+    <v-main>
+      <v-container fluid fill-height class="fondo">
+        
+        <Login v-if="!usuario" @onIngresar="ingresar" @onNotificar="notificar"/>
+        
+      </v-container>
+    </v-main>
+    
+  </v-app>
+</template>
+
+<script>
+import Login from './components/Login';
+
+export default {
+  name: 'App',
+
+  components: {
+    Login,
+  },
+
+  data(){
+    return{
+      usuario: null,
+      notificacion: {
+        mensaje: "",
+        color: "info",
+        visible: false,
+        tiempo: 0,
+      },
+      email: false
+    } 
+  },
+  methods: {
+    
+  }
+};
+</script>
+
+<style>
+.fondo{
+  background-color: #f1f1f1;
+}
+</style>
---
+++ front/src/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+        <v-layout justify-center align-center>
+                <v-flex xs12 sm8 md6 lg5 xl4>
+
+
+
+                </v-flex>  
+        </v-layout>
+</template>
+
+<style>
+
+</style>
