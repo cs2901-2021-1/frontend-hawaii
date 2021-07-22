3e88d3d840d956560baca405c72f01ef8d6a0858
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-07-22 13:11:47 -0500

    Update Proyecciones.vue

--- src/components/Proyecciones.vue
+++ src/components/Proyecciones.vue
@@ -76,7 +76,10 @@ export default {
 
         methods: {
                 administrar(){
-                        window.location.href = "https://api.cs.mrg.com.pe/api-sec02-group02/ti/auth";
+                        //window.location.href = "https://api.cs.mrg.com.pe/api-sec02-group02/ti/auth";
+                        axios.get("https://api.cs.mrg.com.pe/api-sec02-group02/ti/auth", {withCredentials: true})
+                        .then(()=> alert("SE PUDO"))
+                        .catch(() => alert("ERROR"))
                 }
         },
 
