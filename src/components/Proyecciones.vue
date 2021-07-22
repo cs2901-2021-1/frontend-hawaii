52493ad1c45d14b3432cf5791f76d756cc30ed69
XChanitoX <40049050+XChanitoX@users.noreply.github.com>
2021-07-22 13:32:25 -0500

    Fixing search

--- src/components/Proyecciones.vue
+++ src/components/Proyecciones.vue
@@ -16,7 +16,7 @@
                                                 <v-container grid-list-md text-xs-center>
                                                         <v-layout row wrap>
                                                                 <v-flex xs4>
-                                                                        <v-text-field class="text-xs-center" v-model="filters.codigo" append-icon="search" label="Código" single-line hide-details></v-text-field>
+                                                                        <v-text-field class="text-xs-center" v-model="filters" append-icon="search" label="Código" single-line hide-details></v-text-field>
                                                                 </v-flex>
                                                                 <v-flex xs4>
                                                                         <v-text-field class="text-xs-center" v-model="filters.nombre" append-icon="search" label="Nombre" single-line hide-details></v-text-field>
@@ -66,10 +66,8 @@ export default {
                                 { text: 'Proyección', value: 'nStudent' },
                                 { text: 'Error (+/-)', value: 'error' },
                         ],
-                        filters: {
-                                codigo: '',
-                                nombre: '',
-                        },
+                        filters: '',
+                        
                         
                 }
         },
